3db2d336ddac9ed87dd0f80cefd28513c04e526b
Fabio Traverso <fabio.traverso@gmail.com>
2017-10-14 18:37:22 -0300

    fix navbar

--- arquitran-web-app/src/components/Navbar.js
+++ arquitran-web-app/src/components/Navbar.js
@@ -8,11 +8,11 @@ class Navbar extends Component {
     if (localStorage.getItem("logged_in") === 'true'){
       return (
         <nav className="navbar navbar-toggleable-md navbar-light bg-faded navbot">
-          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div><NavLink className="navbar-brand" to="/">Arquitran App</NavLink></div>
-          <div className="collapse navbar-collapse">
+          <div className="collapse navbar-collapse" id="navbarToggler">
             <ul className="navbar-nav mr-auto mt-2 mt-md-0">
               <li className="nav-item">
                 <NavLink exact className="nav-link" activeClassName="active" to="/">Home</NavLink>
@@ -24,18 +24,18 @@ class Navbar extends Component {
                 <NavLink className="nav-link" activeClassName="active" to="/cart">{`Cart (${this.props.total_cart})`}</NavLink>
               </li>
             </ul>
-            <button className="btn btn-outline-danger" type="submit" onClick={this.props.logOut}>Log Out</button>
+            <button className="btn btn-outline-danger right-btn-menu" type="submit" onClick={this.props.logOut}>Log Out</button>
           </div>
         </nav>
       );
     }
     return(
       <nav className="navbar navbar-toggleable-md navbar-light bg-faded navbot">
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div><NavLink className="navbar-brand" to="/">Arquitran App</NavLink></div>
-        <div className="collapse navbar-collapse">
+        <div className="collapse navbar-collapse" id="navbarToggler">
           <ul className="navbar-nav mr-auto mt-2 mt-md-0">
             <li className="nav-item">
               <NavLink exact className="nav-link" activeClassName="active" to="/">Home</NavLink>
@@ -44,7 +44,7 @@ class Navbar extends Component {
               <NavLink className="nav-link" activeClassName="active" to="/products">Products</NavLink>
             </li>
           </ul>
-          <button className="btn btn-outline-primary" type="submit"><NavLink to="/signin">Sign In</NavLink></button>
+          <button className="btn btn-outline-primary right-btn-menu" type="submit"><NavLink to="/signin">Sign In</NavLink></button>
         </div>
       </nav>
     );

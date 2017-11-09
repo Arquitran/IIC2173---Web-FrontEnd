fcd8c178b36e0ebaed20d09d9ca12b4df8a1db94
Francisco <fjvaldes1@uc.cl>
2017-11-08 23:18:34 -0300

    fix auth

--- arquitran-web-app/src/index.js
+++ arquitran-web-app/src/index.js
@@ -5,10 +5,11 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 export const URL_CATEGORIES = "http://arqss17.ing.puc.cl:3000/categorias";
-export const URL_PRODUCTS = "https://private-edb58d-iic2113.apiary-mock.com/api/products";
-export const URL_SIGNUP = "https://private-edb58d-iic2113.apiary-mock.com/api/signup";
-export const URL_SIGNIN = "https://private-edb58d-iic2113.apiary-mock.com/api/signin";
-export const URL_CART = "https://private-edb58d-iic2113.apiary-mock.com/api/cart"
+export const URL_PRODUCTS = "http://arqss17.ing.puc.cl:3000/products";
+export const URL_SIGNUP = "https:///arqss4.ing.puc.cl/api/signup";
+export const URL_SIGNIN = "https:///arqss4.ing.puc.cl/api/signin";
+export const URL_CART = "https://arqss5.ing.puc.cl/api/cart";
+export const URL_HISTORY = "https://arqss5.ing.puc.cl/api/cart/history"
 export const MAX_PAGES = 2;
 
 ReactDOM.render(<App />, document.getElementById('root'));
